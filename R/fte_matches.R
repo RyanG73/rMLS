322f48ca0dac1e032fdefbbde3f23ace3739f44f
RyanG73 <34326964+RyanG73@users.noreply.github.com>
2021-03-04 14:09:22 -0500

    fte_matches added

---
+++ R/fte_matches.R
@@ -0,0 +1,20 @@
+#' A function to pull fivethirtyeight's match list and probabilities
+#'
+#' This function allows me to test setup
+#' @param fte_matches, defaults to 2017,2020
+#' @keywords fivethirtyeight, fte, matches
+#' @export
+#' @examples
+#' fte_matches()
+
+fte_matches <- function(start_season=2017,end_season=2020){
+  matches <- readr::read_csv("https://projects.fivethirtyeight.com/soccer-api/club/spi_matches.csv")
+  matches <- matches %>%
+    dplyr::filter(league == 'Major League Soccer') %>%
+    dplyr::filter(season >= start_season) %>%
+    dplyr::filter(season <= end_season)
+  return(matches)
+}
+
+
+

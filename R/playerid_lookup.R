15dbe9c0d9ec8b892bf5cc07c7e1449ef6dd4dc0
RyanG73 <34326964+RyanG73@users.noreply.github.com>
2021-03-12 20:28:06 -0500

    test

---
+++ R/playerid_lookup.R
@@ -0,0 +1,13 @@
+#' A function to find the identification number for any player
+#'
+#' This function allows me to test setup
+#' @param playerid_lookup, no default, pulls all players
+#' @keywords playerid, id, player, lookup
+#' @importFrom magrittr "%>%"
+#' @export
+#' @examples \dontrun{playerid_lookup()}
+#' playerid_lookup()
+
+playerid_lookup <- function(){
+  print('a')
+}
